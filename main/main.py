cf1d786b87d45f498ecab84071b11768873cf1f9
David McInnis <davidm@eagles.ewu.edu>
2015-04-09 12:27:15 -0700

    replaced images with Jpeg from MET

--- main/main.py
+++ main/main.py
@@ -2,7 +2,10 @@ __author__ = 'David McInnis'
 
 
 
-from skimage import data as skdata
-from skimage import io as skio
+
+import skimage.io as skio
 from skimage import filters as skfilters
 
+image = skio.imread('i1.jpg')
+
+skio.imshow(image)
