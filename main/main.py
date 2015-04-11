612ac3a1b5b26f7304973d0f85d02498aecba58b
David McInnis <davidm@eagles.ewu.edu>
2015-04-11 14:07:52 -0700

    Test out pywt

--- main/main.py
+++ main/main.py
@@ -2,11 +2,14 @@ __author__ = 'David McInnis'
 
 
 import skimage.io as skio
+import numpy, pywt
 
-image = skio.imread('i1.jpg') #http://images.metmuseum.org/CRDImages/as/original/DP118649_CRD.jpg
+# image = skio.imread('i1.jpg', True) #http://images.metmuseum.org/CRDImages/as/original/DP118649_CRD.jpg
+
+print(pywt.wavelist())
 
 
 
 
-skio.imshow(image)
-skio.show()
+# skio.imshow(image)
+# skio.show()
