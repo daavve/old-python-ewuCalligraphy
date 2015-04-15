f213683eea3104150a9a4bc6294961ba76e8956c
David McInnis <davidm@eagles.ewu.edu>
2015-04-14 23:21:57 -0700

    Added some stuff for pywt

--- main/main.py
+++ main/main.py
@@ -1,15 +1,4 @@
 __author__ = 'David McInnis'
 
 
-import skimage.io as skio
-import numpy, pywt
-
-# image = skio.imread('i1.jpg', True) #http://images.metmuseum.org/CRDImages/as/original/DP118649_CRD.jpg
-
-print(pywt.wavelist())
-
-
-
-
-# skio.imshow(image)
-# skio.show()
+runpywt()
---
+++ main/runscikit.py
@@ -0,0 +1,9 @@
+__author__ = 'David McInnis'
+
+import skimage.io as skio
+
+image = skio.imread('i1.jpg', True) #http://images.metmuseum.org/CRDImages/as/original/DP118649_CRD.jpg
+
+
+skio.imshow(image)
+skio.show()
