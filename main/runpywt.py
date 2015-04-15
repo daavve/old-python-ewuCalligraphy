d9597c97e9b7ea017b801cec0f3358db65443b7c
David McInnis <davidm@eagles.ewu.edu>
2015-04-15 16:55:51 -0700

    Went on to iterate through wavelets

--- main/runpywt.py
+++ main/runpywt.py
@@ -9,31 +9,35 @@ import pywt
 # Load image
 original = ndimage.imread('i1.jpg', mode='L')
 
-# Wavelet transform of image, and plot approximation and details
-titles = ['Approximation', ' Horizontal detail',
-          'Vertical detail', 'Diagonal detail']
-coeffs2 = pywt.dwt2(original, 'bior1.3')
-LL, (LH, HL, HH) = coeffs2
-fig = plt.figure()
-for i, a in enumerate([LL, LH, HL, HH]):
-    ax = fig.add_subplot(2, 2, i + 1)
-    ax.imshow(a, origin='image', interpolation="nearest", cmap=plt.cm.gray)
-    ax.set_title(titles[i], fontsize=12)
+waveList = pywt.wavelist()
 
-fig.suptitle("dwt2 coefficients", fontsize=14)
-
-# Now do the same with dwtn/idwtn, to show the difference in their signatures
-
-coeffsn = pywt.dwtn(original, 'bior1.3')
-fig = plt.figure()
-for i, key in enumerate(['aa', 'ad', 'da', 'dd']):
-    ax = fig.add_subplot(2, 2, i + 1)
-    ax.imshow(coeffsn[key], origin='image', interpolation="nearest", cmap=plt.cm.gray)
-    ax.set_title(titles[i], fontsize=12)
-
-fig.suptitle("dwtn coefficients", fontsize=14)
-
+for a in waveList:
+    wavelet = pywt.Wavelet(a)
+    LL, (LH, HL, HH) = pywt.dwt2(original, a, mode='sym')
+    fig = plt.figure()
+    for i, a in enumerate([LL, LH, HL, HH]):
+        ax = fig.add_subplot(2, 2, i + 1)
+        ax.imshow(a, origin='image', interpolation="nearest", cmap=plt.cm.gray)
+    plt.show()
+    input("Press enter to contiune")
+    plt.close()
 
 plt.show()
+
+
+# Wavelet transform of image, and plot approximation and details
+#titles = ['Approximation', ' Horizontal detail',
+#          'Vertical detail', 'Diagonal detail']
+#coeffs2 = pywt.dwt2(original, 'bior1.3')
+#LL, (LH, HL, HH) = coeffs2
+#fig = plt.figure()
+#for i, a in enumerate([LL, LH, HL, HH]):
+#    ax = fig.add_subplot(2, 2, i + 1)
+#    ax.imshow(a, origin='image', interpolation="nearest", cmap=plt.cm.gray)
+#    ax.set_title(titles[i], fontsize=12)
+
+#fig.suptitle("dwt2 coefficients", fontsize=14)
+
+#plt.show()
 
 
