8420de4478852b1fd0c76e8ead18d1c203f4e059
David McInnis <davidm@eagles.ewu.edu>
2015-05-05 14:06:07 -0700

    Added new file for Rest Easy

--- main/runRestEasy.py
+++ main/runRestEasy.py
@@ -1,3 +1,5 @@
+from rest_easy.core.source import SourceBuilder
+
 __author__ = 'David McInnis'
 
 
@@ -5,5 +7,6 @@ __author__ = 'David McInnis'
 from rest_easy.core.main import RestEasy
 
 RestEasy = RestEasy()
-
+dpla = RestEasy.get_wrappers("dpla")
+dpla("v2").apiKey()
 
