f17586c742f070d89e6fe8cc162caa9b682f688e
David McInnis <davidm@eagles.ewu.edu>
2015-05-09 09:13:53 -0700

    Now had test dlpa query ability

--- main/runRestEasy.py
+++ main/runRestEasy.py
@@ -8,5 +8,10 @@ from rest_easy.core.main import RestEasy
 
 RestEasy = RestEasy()
 dpla = RestEasy.get_wrappers("dpla")
-dpla("v2").apiKey()
+dpla("v2").apiKey("de61f325217108a6de996ea4fcf64092")
+dpla('v2').Items.searchIn.title('calligraphy')
+dpla('v2').Items.searchIn.language.name('chinese')
+results = dpla('v2').Items.GET()
 
+
+print (results[0])
