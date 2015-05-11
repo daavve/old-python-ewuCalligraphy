65b94c9faa05d13c2b3d402ce0b49cce5e6b98c3
David McInnis <davidm@eagles.ewu.edu>
2015-05-11 15:27:19 -0700

    push to laptop

--- main/runscikit.py
+++ main/runscikit.py
@@ -2,7 +2,7 @@ __author__ = 'David McInnis'
 
 import skimage.io as skio
 
-image = skio.imread('i1.jpg', True) #http://images.metmuseum.org/CRDImages/as/original/DP118649_CRD.jpg
+image = skio.imread('1.jpg', True) #http://images.metmuseum.org/CRDImages/as/original/DP118649_CRD.jpg
 
 
 skio.imshow(image)
