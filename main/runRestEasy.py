85db181b9bafeee2343483279f99e6f7276f1742
David McInnis <davidm@eagles.ewu.edu>
2015-05-10 17:01:55 -0700

    dpla api may not provide sufficient details to justify expense of learning it

--- main/runRestEasy.py
+++ main/runRestEasy.py
@@ -9,8 +9,9 @@ from rest_easy.core.main import RestEasy
 RestEasy = RestEasy()
 dpla = RestEasy.get_wrappers("dpla")
 dpla("v2").apiKey("de61f325217108a6de996ea4fcf64092")
-dpla('v2').Items.searchIn.title('calligraphy')
+dpla('v2').Items.searchIn.title('Wei Shuo')
 dpla('v2').Items.searchIn.language.name('chinese')
+# dpla('v2').Items.searchIn.limit('100')  #Not sure how to use search limit
 results = dpla('v2').Items.GET()
 
 
