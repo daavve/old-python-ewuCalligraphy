fabde2c91e608176d6ee32a1437b46e1cce45b7b
David McInnis <davidm@eagles.ewu.edu>
2015-05-12 16:52:31 -0700

    Talked to Kosuke, he wants me to run the first part of the wavlet transform multiple times.

--- main/runpywt.py
+++ main/runpywt.py
@@ -7,22 +7,20 @@ import pywt
 
 
 # Load image
-original = ndimage.imread('i1.jpg', mode='L')
+original = ndimage.imread('2.jpg', mode='L')
 
 waveList = pywt.wavelist()
-
 for a in waveList:
     wavelet = pywt.Wavelet(a)
     LL, (LH, HL, HH) = pywt.dwt2(original, a, mode='sym')
     fig = plt.figure()
-    for i, a in enumerate([LL, LH, HL, HH]):
-        ax = fig.add_subplot(2, 2, i + 1)
-        ax.imshow(a, origin='image', interpolation="nearest", cmap=plt.cm.gray)
+    for ii in range(1,7):
+        ax = fig.add_subplot(2, 3, ii)
+        ax.imshow(LL, origin='image', interpolation="nearest", cmap=plt.cm.gray)
+        LL, (LH, HL, HH) = pywt.dwt2(LL, a, mode='sym')
     plt.show()
-    input("Press enter to contiune")
-    plt.close()
 
-plt.show()
+
 
 
 # Wavelet transform of image, and plot approximation and details
